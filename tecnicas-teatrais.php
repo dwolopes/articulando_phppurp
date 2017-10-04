ee38e1dd8bc362e09ee45339429336f1175f322d
Breno Pereira Magalhães Silva <brenosilva1@gmail.com>
2017-10-04 19:05:47 -0300

    Rafael : Adcionado quarto post e modificado no index / breno: criado contato em contrucao

---
+++ tecnicas-teatrais.php
@@ -0,0 +1,187 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Articulei</title>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">  
+  <link href="fonts/Bariol.ttf" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="img/icon.png">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+  <script src="js/style.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+    
+
+  <link rel="stylesheet" type="text/css" href="post1.css">
+</head>
+<body>
+
+
+<nav class="navbar navbar-default">
+      <div class="container clear">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img style="max-width:50px; margin-top: -25px;" src="img/teste.png"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="index.php"">HOME</a></li>
+              <li><a href="index.php"">SOBRE</a></li>
+              <li><a href="index.php"">CONTATO</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    
+
+
+    <header class="masthead" style="background-image: url('https://imageshack.com/i/pow3JDkZp")>
+      <div class="container clear">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto col-lg-offset-2 col-md-offset-1">
+            <div class="post-heading">
+              <h1>Conheça As 5 Técnicas Teatrais Que Executivos Usam Ao Falar Em Público</h1>
+              <span class="meta">Por: Equipe Articulei</span>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+ <article>
+      <div class="container clear">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto col-lg-offset-2 col-md-offset-1">
+
+            <h4 class="section-heading">  Fala constantemente em público no seu trabalho? Tem dificuldades em se expressar nas apresentações? </h4>
+
+            <p>Você sabia que vários executivos estão optando pelo teatro?</p>
+
+            <p>Sim, o teatro; Por desenvolver técnicas para solucionar o medo e a vergonha de falar no ambiente corporativo. É justamente esse assunto que iremos abordar neste artigo. Portanto, se lhe pareceu interessante até aqui, vem com a gente e descubra novas possibilidades.</p>
+            
+            <p>ARTE NO CORPORATIVO </p>
+            
+            <p>Falar em público, apresentar um projeto, são ofícios presentes no dia-a-dia de um executivo.</p> 
+
+            <p>Para muitos essas tarefas são vistas como um desafio, a timidez e o medo do julgamento alheio levam os profissionais a não expor o máximo de si, escondendo sua real personalidade e seus conhecimentos. </p>
+
+            <p>Ao se apresentar em público no trabalho, seja para transmitir uma mensagem, apresentar um projeto ou os seus resultados; Sua voz fica trêmula, dá aquele frio na barriga, falta de ar, fica andando de um lado para o outro, não sabe onde colocar as mãos, se perde nas palavras e quando precisa improvisar acaba se perdendo todo e travando? </p>
+
+            <p>Todos esses fatores citados anteriormente podem ser trabalhados com técnicas teatrais. Se comunicar bem no ambiente corporativo hoje é fundamental para a evolução do profissional e seu crescimento dentro da empresa. </p>
+
+            <p>As técnicas teatrais surgem como ferramentas para potencializar o profissional, fazendo com que ele esteja presente em sua apresentação de forma viva e segura.</p>
+
+            <p>Elas que são usadas, normalmente, para fins artísticos, acabam então se tornando uma maneira de preparar o executivo para se comunicar bem, através de um autoconhecimento dos seus sentidos, gestos e ações. Podendo assim estar melhor preparado para enfrentar os desafios impostos no dia-a-dia corporativo.</p>
+
+
+
+            <h4>1 - VOZ / RESPIRAÇÃO</h4>
+            <a href="#">
+              <img class="img-fluid" src="https://imageshack.com/i/pmBPJXXHp" alt="">
+            </a>
+
+            <p>Os exercícios de voz trabalham a maneira de pronunciar palavras, a projeção da fala em um volume e entonação ideal para cada momento, além da articulação verbal, tornando-a mais clara, precisa e objetiva.
+            Exercícios de respiração surgem como um complemento para a voz, uma boa respiração ajuda o profissional a falar com mais clareza, expor o real sentido e objetivo da frase dita. </p>
+
+            <h4>2- CORPO</h4>
+
+            <a href="#">
+              <img class="img-fluid" src="https://imageshack.com/i/pogFgXPEp" alt="">
+            </a>
+
+            <p>Os exercícios corporais ajudam a ter uma melhor consciência, trazendo  movimentos mais objetivos, para transparecer segurança. Com o controle sobre os gestos, sabendo quais deve fazer e quais deve evitar para que a mensagem seja a mais clara possível.</p>
+
+            <h4>3- EMOCIONAL</h4>
+
+            <a href="#">
+              <img class="img-fluid" src="https://imageshack.com/i/pmCV5bG8p" alt="">
+            </a>
+
+            <p>Exercícios para o emocional ajudam o profissional a controlar sua ansiedade, nervosismo e a vergonha. Passando mais segurança e tranquilidade na hora da fala, fazendo com que a mensagem seja melhor transmitida e captada.</p>
+
+            <h4>4- IMPROVISO </h4>
+
+            <a href="#">
+              <img class="img-fluid" src="https://imageshack.com/i/pmCAAH2bp" alt="">
+            </a>
+
+            <p>A improvisação é uma ferramenta importantíssima para o ator, pois ela desenvolve a capacidade de resolver o inesperado. Dentro das empresas, as técnicas que os executivos aprendem com o teatro, podem ajudar a resolver de forma criativa e espontânea determinados imprevistos em uma apresentação, ou em tarefas na empresa. Essa técnica fará com que você desenvolva o famoso "jogo de cintura", tão almejado em ambientes corporativos.</p>
+
+            <h4>5- AUTOCONHECIMENTO</h4>
+
+            <a href="#">
+              <img class="img-fluid" src="https://imageshack.com/i/poLCiktsp" alt="">
+            </a>
+
+            <p>Ao colocar em prática todos os ensinamentos citados anteriormente, o profissional se torna um melhor conhecedor de si próprio, tanto do seu corpo quanto do seu inconsciente. Uma pessoa que se conhece melhor, consequentemente é um profissional ciente da suas capacidades, ações e mais seguro para agir em momentos de imprevisibilidade.</p>
+
+            <h6>Fontes: https://globoplay.globo.com/v/5943512/
+                http://glamurama.uol.com.br/poderosos-procuram-cursos-de-teatro-para-adotar-discurso-mais-descontraido/<h6>
+
+
+          </div>
+        </div>
+      </div>
+    </article>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
+    
+
+<footer>
+<div class="container-fluid">
+    <div class="row">
+        <div class="search-text"> 
+            <div class="container">
+                <div class="row text-center">
+                    <img id="divCorpo" class="img-responsive" width="20">
+                    <div id="obrigado_rodape"></div>
+                    <div class="form-inline">
+                        <h5>Quer adquirir cada vez mais conhecimento?</h5>
+                        <h4 class="cham">Seja sempre o primeiro a receber nossas novidades</h4>
+                        <form action="armazenamento.php" method="POST" id="form_rodape">
+                            <div class="col-md-3 col-md-offset-3 " >
+                                <div class="input-group" style="min-width: 200px">
+                                    <input type="text" class="form-control" name="nome_rodape" placeholder="Nome completo" required style="border-radius: 5px; margin-bottom: 5px;">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="input-group" style="min-width:300px">
+                                    <input type="email" class="form-control" name="email_rodape" placeholder="Email" required style="margin-bottom: 5px;">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-secondary" type="submit" style="margin-bottom: 5px;"><strong>Quero receber </strong> </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>         
+        </div> 
+        <div class="copyright">
+            <div class="container">
+                <div class="row text-center">
+                    <p>Copyright © 2017 All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    </div> 
+    </div> 
+</footer>
+
+</body>
+</html>
