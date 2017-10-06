2d254aa351ef33ff3b1934ae91d8f374a9b56645
Breno Pereira Magalhães Silva <brenosilva1@gmail.com>
2017-10-06 15:31:04 -0300

    adicionado sexto e setimo post e seus links no index

---
+++ pitch.php
@@ -0,0 +1,170 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Articulei</title>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">  
+  <link href="fonts/Bariol.ttf" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="img/icon.png">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+  <script src="js/style.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+    
+
+  <link rel="stylesheet" type="text/css" href="css/post1.css">
+</head>
+<body>
+
+
+<nav class="navbar navbar-default">
+      <div class="container clear">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php"><img style="max-width:50px; margin-top: -25px;" src="img/teste.png"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="index.php"">HOME</a></li>
+              <li><a href="sobre.php"">SOBRE</a></li>
+              <li><a href="contato.php"">CONTATO</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    
+
+
+    <header class="masthead" style="background-image: url(http://imageshack.com/a/img923/7205/T8fOR7.png")>
+      <div class="container clear">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto col-lg-offset-2 col-md-offset-1">
+            <div class="post-heading">
+              <h1>Dicas de como fazer pitch de vendas com excelência</h1>
+              <span class="meta">Por: Equipe Articulei</span>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+ <article>
+      <div class="container clear">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto col-lg-offset-2 col-md-offset-1">
+
+            <h4 class ="section-heading" >A comunicação é tudo dentro de um negócio. Conheça dicas de como fazer um picht com excelência. </h4> 
+
+            <p>Nada adianta se você possuir uma empresa com boa infraestrutura, oferecer serviços ou produtos de qualidade, possuir um bom atendimento, profissionais treinados se não conter em todo esse pacote o fator mais importante para que as vendas ocorram – A comunicação!</p>
+
+
+            <p>Uma boa comunicação é essencial dentro de qualquer negócio, só assim a mensagem que a empresa quer passar chegará a seu Público Alvo da melhor maneira, resultando em mais vendas. Quanto mais se sabe realizar uma boa comunicação com os clientes, mais eficiente será e fazer um pitch é uma das maneiras de trabalhar uma boa comunicação dentro de um negócio. </p>
+
+            <p>
+              Uma fase que todos os empreendimentos passam, é a fase de pouco tempo para vender. “Tempo é dinheiro”, essa frase faz todo o sentido e um <strong>pitch</strong> nestes momentos pode trazer bom resultados. 
+            </p>
+
+            <h4>
+              <strong>Dicas de como fazer pitch de vendas com excelência</strong>
+            </h4>
+
+            <p>
+              Com algumas dicas práticas é possível fazer um pitch de vendas com excelência e ainda melhorar seu negócio no mercado de atuação, expandindo, aumentando vendas e trazendo mais clientes. Iremos deixar aqui algumas dicas de como fazer um pitch de vendas com excelência: 
+            </p>
+
+
+            <h4> <strong>Necessidade, problema e solução</strong></h4>
+
+
+            <p>Você possui um produto ou serviço e agora precisa vendê-lo, para isto é preciso ter conhecimento das necessidades de seu público, os problemas deles e como seu produto ou serviço pode ser solução. Saiba aproveitar as necessidades e mostrar as pessoas seu produto ou serviço, e é possível que elas nem tivessem conhecimento e passam a serem clientes</p>
+
+
+            <h4> <strong>Posicionamento de mercado</strong></h4>
+
+
+            <p>Saiba posicionar seu produto ou serviço dentro do mercado de atuação, e saiba mostrar força dentro deste mercado. Se mostre para o cliente da melhor forma o possível, encantando-o</p>
+
+            <h4> <strong>Expectativas</strong></h4>
+
+
+            <p> Os clientes procuram sempre crescer no futuro e buscar coisas a mais, então faça com que eles crie expectativas em relação aos seus produtos ou serviços</p>
+
+            <h4> <strong>Deixe suas intenções de forma clara</strong></h4>
+
+
+            <p>Não faça promessas que seu produto ou serviço não vá cumprir, não engane seus clientes. Sempre deixe muito claro quais suas intenções</p>
+
+
+            h4> <strong>Credibilidade</strong></h4>
+
+
+            <p>Passe sempre credibilidade aos seus clientes, só assim suas vendas vão sempre estar aumentando. </p>
+
+            <p>Fazer um pitch é saber realizar uma boa comunicação, sendo você um empreendedor ou vendedor. A comunicação é o segredo do negócio, então se atente, pois, suas vendas dependem muito dela.</p>
+
+          </div>
+        </div>
+      </div>
+    </article>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
+    
+
+<footer>
+<div class="container-fluid">
+    <div class="row">
+        <div class="search-text"> 
+            <div class="container">
+                <div class="row text-center">
+                    <img id="divCorpo" class="img-responsive" width="20">
+                    <div id="obrigado_rodape"></div>
+                    <div class="form-inline">
+                        <h5>Quer adquirir cada vez mais conhecimento?</h5>
+                        <h4 class="cham">Entre para nossa lista e receba um infográfico incrível e conteúdos com exclusividade</h4>
+                        <form action="armazenamento.php" method="POST" id="form_rodape">
+                            <div class="col-md-3 col-md-offset-3 " >
+                                <div class="input-group" style="min-width: 200px">
+                                    <input type="text" class="form-control" name="nome_rodape" placeholder="Nome completo" required style="border-radius: 5px; margin-bottom: 5px;">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="input-group" style="min-width:300px">
+                                    <input type="email" class="form-control" name="email_rodape" placeholder="Email" required style="margin-bottom: 5px;">
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-secondary" type="submit" style="margin-bottom: 5px;"><strong>Quero receber </strong> </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>         
+        </div> 
+        <div class="copyright">
+            <div class="container">
+                <div class="row text-center">
+                    <p>Copyright © 2017 All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    </div> 
+    </div> 
+</footer>
+
+</body>
+</html>
